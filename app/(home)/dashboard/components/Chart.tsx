82fc4df80f3645a3331d6a2e79116ad9171d22fd
rezadrian01 <ahmadadrian324@gmail.com>
2024-12-23 09:14:02 +0700

    feat: create chart component using dummy data on dashboard page

---
+++ app/(home)/dashboard/components/Chart.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import { Pie, PieChart } from "recharts"
+
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "@/components/ui/chart"
+import { FC } from "react"
+
+const chartConfig = {
+    books: {
+        label: "Books"
+    },
+    borrow: {
+        label: "Borrowed",
+        color: "#3D3E3E",
+    },
+    return: {
+        label: "Returned",
+        color: "#151619",
+
+    },
+} satisfies ChartConfig
+
+export interface BooksData {
+    borrow: number,
+    return: number
+}
+
+interface ChartProps {
+    booksData: BooksData
+}
+
+export const Chart: FC<ChartProps> = ({ booksData }) => {
+
+    const chartData = [
+        { status: "borrow", books: booksData.borrow, fill: "var(--color-borrow)" },
+        { status: "return", books: booksData.return, fill: "var(--color-safari)" },
+    ]
+
+
+    return (
+        <ChartContainer
+            config={chartConfig}
+            className="mx-auto aspect-square max-h-[50rem]"
+        >
+            <PieChart>
+                <ChartTooltip
+                    cursor={false}
+                    content={<ChartTooltipContent hideLabel />}
+                />
+                <Pie data={chartData} dataKey="books" nameKey="status" />
+            </PieChart>
+        </ChartContainer>
+    )
+}
--- app/(home)/dashboard/page.tsx
+++ app/(home)/dashboard/page.tsx
@@ -1,13 +1,41 @@
 import { Button } from '@/components/ui/button'
 import React from 'react'
 import { auth } from '@/auth'
+import { BooksData, Chart } from './components/Chart';
+
+import blackLogo from "@/public/svg/blackLogo.svg";
+import Image from 'next/image';
 
 const DashboardPage = async () => {
     const session = await auth();
-    return (
-        <div className=''>
-            DashboardPage
 
+    const BOOKS_DATA: BooksData = {
+        borrow: 100,
+        return: 150
+    }
+
+    return (
+        <div className='grid grid-cols-2 p-10'>
+            <div className='col-span-1'>
+                <Chart booksData={BOOKS_DATA} />
+
+                <div className='bg-slate-50 rounded-lg flex gap-4 items-center p-4 max-w-[25rem] mx-auto'>
+                    <Image width={45} src={blackLogo} alt='Black logo' />
+                    <div className='border-l-2 border-l-[#151619] space-y-4 px-6'>
+                        <div className='flex gap-4'>
+                            <div className='w-6 aspect-square bg-[#3D3E3E] rounded-full' />
+                            <h5>Total Borrowed Books</h5>
+                        </div>
+                        <div className='flex gap-4'>
+                            <div className='w-6 aspect-square bg-[#151619] rounded-full' />
+                            <h5>Total Returned Books</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='col-span-1'>
+                Statistic
+            </div>
         </div>
     )
 }
